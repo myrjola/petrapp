456d33202035567804173533fd4f8ca469b1ae91
Martin Yrjölä <martin.yrjola@gmail.com>
2025-07-29 23:00:02 +0300

    Formatting

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -16,7 +16,8 @@ func (app *application) routes() *http.ServeMux {
 	}
 
 	session := func(next http.Handler) http.Handler {
-		return app.recoverPanic(noCache(app.sessionManager.LoadAndSave(app.webAuthnHandler.AuthenticateMiddleware(shared(next)))))
+		return app.recoverPanic(noCache(app.sessionManager.LoadAndSave(
+			app.webAuthnHandler.AuthenticateMiddleware(shared(next)))))
 	}
 
 	mustSession := func(next http.Handler) http.Handler {
